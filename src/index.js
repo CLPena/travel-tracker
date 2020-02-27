fd696feffc9c45b4c89c1dad8fd0d797a1273681
Cristina Pena <cristinalpena@gmail.com>
2020-02-26 19:36:27 -0700

    Finish adjusting icon placement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import domUpdates from './domUpdates';
 import $ from 'jquery';
 import './css/base.scss';
-// import './css/all.css';
 import './images/login-background.jpg';
-
-console.log('This is the JavaScript entry file - your code begins here.');
+import './images/compass.png';
+import './images/pin.png';
