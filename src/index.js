edaace209334d7b06d72bda0ae2d79bb9eaed5f1
Cristina Pena <cristinalpena@gmail.com>
2020-02-26 20:36:38 -0700

    Add new class files and initial fetch structure

--- src/index.js
+++ src/index.js
@@ -5,20 +5,47 @@ import './images/login-background.jpg';
 import './images/compass.png';
 import './images/pin.png';
 
+// FETCH DATA //
+let travelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+  .then(response => response.json())
+  .then(data => data.travelers)
+  .catch(error => console.log(error.message))
+
+let tripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
+  .then(response => response.json())
+  .then(data => data.trips)
+  .catch(error => console.log(error.message));
+
+let destinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+  .then(response => response.json())
+  .then(data => data.destinations)
+  .catch(error => console.log(error.message));
+
+Promise.all([travelersData, tripsData, destinationsData])
+  .then(data => {
+    travelersData = data[0];
+    tripsData = data[1];
+    destinationsData = data[2];
+
+    loginSubmitButton.addEventListener('click', determineIfAgent);
+    // loginSubmitButton.addEventListener('keyup', determineKey);
+  })
+  .catch(error => console.log(error.message))
+
+// DECLARE VARIABLES //
+let traveler, trip, destination, agency;
 
 let loginMain = document.querySelector('.login-screen');
 let loginSubmitButton = document.querySelector('.login-button');
 let usernameInput = document.querySelector('.username-input');
 let passwordInput = document.querySelector('.password-input');
 
-loginSubmitButton.addEventListener('click', determineIfAgent);
-loginSubmitButton.addEventListener('keyup', determineKey);
 
-function determineKey(event) {
-  if(event.keyCode === 13) {
-    determineIfAgent();
-  }
-}
+// function determineKey(event) {
+//   if(event.keyCode === 13) {
+//     determineIfAgent();
+//   }
+// }
 
 function determineIfAgent() {
   if(usernameInput.value === 'agency' && passwordInput.value === 'travel2020') {
