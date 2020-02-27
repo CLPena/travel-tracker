ca6ce07e456ba8d2545681c19de1f65a5941112e
Cristina Pena <cristinalpena@gmail.com>
2020-02-26 18:37:20 -0700

    Add domUpdates file, edit icons, adjust styling slightly

---
+++ src/domUpdates.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+let domUpdates = {
+  showTravelerDashboard(){
+    //if incorrect input apply error class, clear input for that field, remove hidden class from error message above field 
+  },
+
+  showAgentDashboard(){
+
+  }
+
+}
